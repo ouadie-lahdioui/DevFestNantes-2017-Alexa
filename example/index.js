bc566d0cd31d4f8b9e9bcf9d12e786590d56d6c5
Ouadie Lahdioui <ouadiesoft@gmail.com>
2017-10-20 19:13:09 +0200

    Retouche

--- example/index.js
+++ example/index.js
@@ -1,41 +1,29 @@
 'use strict';
 
 const Alexa = require("alexa-sdk");
-const jobs = require("./jobs");
+const Talks = require("./talks");
 
 const handlers = {
     'LaunchRequest': function () {
-        this.attributes.speechOutput = "Welcome to Joby, the job search skill to find your next career opportunity. You can ask a question like, what are the latest jobs offers in Paris ? Now, how can i help you ?";
+        this.attributes.speechOutput = "Welcome to DevFest Nantes 2017. The biggest DevFest in europe carefully crafted for you by GDG community ! Be a Coder, Be a Hero ! Now, how can i help you ?";
         this.attributes.repromptSpeech = "I'm still waiting for your question";
         this.emit(':ask', this.attributes.speechOutput, this.attributes.repromptSpeech);
     },
-    'JobSearchIntent': function () {
-        let cityName;
-        const citySlot = this.event.request.intent.slots.city;
-        
-        if (citySlot && citySlot.value) {
-            cityName = citySlot.value.toUpperCase();
-        }
-
-        const job = jobs[cityName];
-
-        if (job) {
-            this.attributes.speechOutput = job.description;
-            this.attributes.repromptSpeech = "if you are interested i can tell you more about the offer ?";
-        } else {
-            let speechOutput = "I'm sorry, i currently do not know";
-            const repromptSpeech = "Can you repeat your question in other words ?";
-        }
-
-        this.emit(':ask', this.attributes.speechOutput, this.attributes.repromptSpeech);
-    },
-    'AMAZON.HelpIntent': function () {
+    'Unhandled': function () {
         this.attributes.speechOutput = "You can ask questions such as, what are the latest jobs offers in Paris, or, you can simple say exit now, how can i help you ?";
         this.attributes.repromptSpeech = "I'm still waiting for you question";
         this.emit(':ask', this.attributes.speechOutput, this.attributes.repromptSpeech);
     },
-    'AMAZON.RepeatIntent': function () {
+    "AMAZON.HelpIntent": function () {
+        this.attributes.speechOutput = "Welcome again to DevFest Nantes, You can ask questions such as, what is the next talk in Belem room ";
+        this.attributes.repromptSpeech = "I'm still waiting for you question";
         this.emit(':ask', this.attributes.speechOutput, this.attributes.repromptSpeech);
+    },
+    "AMAZON.RepeatIntent": function() {
+        this.emit(':ask', this.attributes.speechOutput, this.attributes.repromptSpeech);
+    },
+    'SessionEndedRequest': function () {
+        this.emit(':tell', 'Goodbye !');
     },
     'AMAZON.StopIntent': function () {
         this.emit('SessionEndedRequest');
@@ -43,17 +31,24 @@ const handlers = {
     'AMAZON.CancelIntent': function () {
         this.emit('SessionEndedRequest');
     },
-    'SessionEndedRequest': function () {
-        this.emit(':tell', 'Goodbye !');
-    },
-    'Unhandled': function () {
-        this.attributes.speechOutput = "You can ask questions such as, what are the latest jobs offers in Paris, or, you can simple say exit now, how can i help you ?";
-        this.attributes.repromptSpeech = "I'm still waiting for you question";
+    "TalkSearchIntent": function() {
+        const roomSlot = this.event.request.intent.slots.room;
+        
+        this.attributes.speechOutput = "I'm sorry, i currently do not know";
+        this.attributes.repromptSpeech = "Can you repeat your question in other words ?";
+
+        if (roomSlot && roomSlot.value) {
+            let roomName = roomSlot.value.toUpperCase();
+            let talk = Talks[roomName];
+            this.attributes.speechOutput = talk.description;
+            this.attributes.repromptSpeech = "If you are interested, i can tell you more abut the speaker ?";
+        }
+
         this.emit(':ask', this.attributes.speechOutput, this.attributes.repromptSpeech);
     }
 };
 
-exports.handler = function (event, context) {
+exports.handler = (event, context, callback) => {
     const alexa = Alexa.handler(event, context);
     alexa.registerHandlers(handlers);
     alexa.execute();
--- example/jobs.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'PARIS': {
-        "description" : "looking for developers focused on JavaScript, passionate about the product and building great experience for millions of users.",
-    },
-    'LONDON': {
-        "description" : "TicTacDoc is searching for a Javascript developer to join our team. We are growing our frontend team to build the next generation of HR delivery services.",
-    }
-};
